21c1bf038affb099d636206997ef7024adf6b965
amtsai96 <amandatsai96@gmail.com>
2019-07-18 17:27:20 +0800

    Update dataset info & plot

--- CNN_model.py
+++ CNN_model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 #from tensorflow import keras
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten, Dropout
-from tensorflow.keras.layers import BatchNormalization, AveragePooling2D, concatenate, Input
+from tensorflow.keras.layers import GlobalAveragePooling2D, BatchNormalization, AveragePooling2D, concatenate, Input
 from tensorflow.keras.optimizers import Adam
 import numpy as np
 import os
@@ -10,7 +10,8 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"]='2'
 
 def CNN(width, height, depth, classes_num):
     #return VGG16Net(width, height, depth, classes_num)
-    return AlexNet(width, height, depth, classes_num)
+    #return AlexNet(width, height, depth, classes_num)
+    return InceptionV1(width, height, depth, classes_num)
 #################################################
 #Define convolution with batchnromalization
 def Conv2d_BN(x, nb_filter,kernel_size, padding='same',strides=(1,1),name=None):
@@ -128,7 +129,7 @@ def InceptionV1(width, height, depth, classes):
     x = Dense(1000,activation='relu')(x)
     x = Dense(classes,activation='softmax')(x)
     
-    model=Model(input=inpt,output=x)
+    model=Model(inputs=inpt,outputs=x)
     model.compile(optimizer=Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=1e-08),loss = 'categorical_crossentropy',metrics=['accuracy'])
 
     return model
@@ -137,7 +138,7 @@ def InceptionV1(width, height, depth, classes):
 def VGG16Net(width, height, depth, classes):
     model = Sequential()
     
-    model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=(224,224,3),padding='same',activation='relu'))
+    model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=(width,height,depth),padding='same',activation='relu'))
     model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Conv2D(128,(3,2),strides=(1,1),padding='same',activation='relu'))
@@ -162,7 +163,7 @@ def VGG16Net(width, height, depth, classes):
     model.add(Dropout(0.5))
     model.add(Dense(1000,activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(17,activation='softmax'))
+    model.add(Dense(classes,activation='softmax'))
     
     model.compile(optimizer=Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=1e-08),loss = 'categorical_crossentropy',metrics=['accuracy'])
 
--- plot.py
+++ plot.py
@@ -4,22 +4,25 @@ import json
 import os
 
 G_FLAG = True#False
-B_FLAG = False#True
-K_FLAG = False
+C_FLAG = True
+B_FLAG = True
+K_FLAG = True#False
 
-FILES_VER = True
+FILES_VER = False#True
 
-dataf = 'data_all_29572.txt'#'data_all.txt'#input('Enter data file name:')
-out = 'out_GENRE.txt'#input('Enter output data file name:')
-out_b = 'out_BPM.txt'
-out_k = 'out_KEY.txt'
+dataf = input('Enter data file name:')
+out_g = 'dataset_GENRE.txt'#input('Enter output data file name:')
+out_c = 'dataset_CATEGORY.txt'
+out_b = 'dataset_BPM.txt'
+out_k = 'dataset_KEY.txt'
 genres = ['8Bit Chiptune', 'Acid', 'Acoustic', 'Ambient', 'Big Room', 'Blues', 'Boom Bap', 'Breakbeat', 'Chill Out', 'Cinematic', 'Classical', 'Comedy', 'Country', 'Crunk', 'Dance', 'Dancehall', 'Deep House', 'Dirty', 'Disco', 'Drum And Bass', 'Dub', 'Dubstep', 'EDM', 'Electro', 'Electronic', 'Ethnic', 'Folk', 'Funk', 'Fusion', 'Garage', 'Glitch', 'Grime', 'Grunge', 'Hardcore', 'Hardstyle', 'Heavy Metal', 'Hip Hop', 'House', 'Indie', 'Industrial', 'Jazz', 'Jungle', 'Lo-Fi', 'Moombahton', 'Orchestral', 'Pop', 'Psychedelic', 'Punk', 'Rap', 'Rave', 'Reggae', 'Reggaeton', 'Religious', 'RnB', 'Rock', 'Samba', 'Ska', 'Soul', 'Spoken Word', 'Techno', 'Trance', 'Trap', 'Trip Hop', 'Weird']
-
+categories = ['Accordion', 'Arpeggio', 'Bagpipe', 'Banjo', 'Bass', 'Bass Guitar', 'Bass Synth', 'Bass Wobble', 'Beatbox', 'Bells', 'Brass', 'Choir', 'Clarinet', 'Didgeridoo',
+'Drum', 'Flute', 'Fx', 'Groove', 'Guitar Acoustic', 'Guitar Electric', 'Harmonica', 'Harp', 'Harpsichord', 'Mandolin', 'Orchestral', 'Organ', 'Pad', 'Percussion', 'Piano', 'Rhodes Piano', 'Scratch', 'Sitar', 'Soundscapes', 'Strings', 'Synth', 'Tabla', 'Ukulele', 'Violin', 'Vocal', 'Woodwind']
 if FILES_VER:
     folder = input('enter the folder that contains files:')
     files = os.listdir(folder)
 
-tags, bpm, key = dict(), dict(), dict()
+cats, gens, bpm, key = dict(), dict(), dict(), dict()
 with open(dataf, 'r') as i:
     for line in i.readlines():
         if line=="\n": continue
@@ -30,8 +33,17 @@ with open(dataf, 'r') as i:
                     filename = data['index']+'.wav'
                     if filename not in files: continue
                 tt = t.strip('Loops').strip(' ')
-                if tt in tags: tags[tt] += 1
-                else: tags[tt] = 1
+                if tt in gens: gens[tt] += 1
+                else: gens[tt] = 1
+
+            if t.strip('Loops').strip(' ') in categories:
+                if FILES_VER:
+                    filename = data['index']+'.wav'
+                    if filename not in files: continue
+                tt = t.strip('Loops').strip(' ')
+                if tt in cats: cats[tt] += 1
+                else: cats[tt] = 1  
+
             if t.endswith('bpm'):
                 if FILES_VER:
                     filename = data['index']+'.wav'
@@ -40,6 +52,7 @@ with open(dataf, 'r') as i:
                 tt = (str(int(tt)//10*10))
                 if tt in bpm: bpm[tt] += 1
                 else: bpm[tt] = 1
+
             if t.startswith('Key'):
                 if FILES_VER:
                     filename = data['index']+'.wav'
@@ -50,18 +63,18 @@ with open(dataf, 'r') as i:
 if(G_FLAG):
     print('--------------------GENRE--------------------')
     # by Genres
-    ts = [t for t in genres and tags]#[t for t in tags]
+    ts = [t for t in genres and gens]
     s = 5
     y_pos = np.arange(0, len(ts)*s, s)
-    vs = [tags[t] for t in genres and tags]
-    print(len(tags))
-    print(tags)
+    vs = [gens[t] for t in genres and gens]
+    print(len(gens))
+    print(gens)
 
     # save text to file
-    with open(out, 'w') as o:
+    with open(out_g, 'w') as o:
         for j in range(len(genres)):
-            if genres[j] in tags:
-                o.write(genres[j]+','+str(tags[genres[j]])+'\n')
+            if genres[j] in gens:
+                o.write(genres[j]+','+str(gens[genres[j]])+'\n')
             else:
                 o.write(genres[j]+',0\n')
 
@@ -80,37 +93,42 @@ if(G_FLAG):
 
     plt.savefig('looperman_dataset_genres.png', bbox_inches='tight', pad_inches=0.0)
     #plt.show()
+    plt.close()
 
-if(B_FLAG):
-    print('--------------------BPM--------------------')
-    # by BPM
-    b = sorted(bpm.items(), key=lambda k:int(k[0]))
-    ts = [t[0] for t in b]
-    vs = [t[1] for t in b]
-    s = 1
+if(C_FLAG):
+    print('--------------------Categories--------------------')
+    # by categories
+    ts = [t for t in categories and cats]
+    s = 5
     y_pos = np.arange(0, len(ts)*s, s)
-    
-    print(len(bpm))
-    print(bpm)
+    vs = [cats[t] for t in categories and cats]
+    print(len(cats))
+    print(cats)
 
     # save text to file
-    with open(out_b, 'w') as o:
-        for j in bpm: o.write(j+','+str(bpm[j])+'\n')
+    with open(out_c, 'w') as o:
+        for j in range(len(categories)):
+            if categories[j] in cats:
+                o.write(categories[j]+','+str(cats[categories[j]])+'\n')
+            else:
+                o.write(categories[j]+',0\n')
 
-    # Plot - by BPM
+    # Plot - by categories
     plt.figure(figsize=(23, 17), num='Looperman Dataset')
-    plt.bar(ts, vs, align='center', alpha=0.5, color='red')
-    for a, b in zip(y_pos, vs):plt.text(a, b+2, str(b), ha='center')
-    #plt.yticks(y_pos, ts)
-    plt.ylabel('Data #')
-    plt.xlabel('BPM')
-    plt.ylim([0,max(vs)+1])
-    plt.xlim([-2,y_pos[-1]+2.3])
-    plt.tick_params(axis='x', which='major', labelsize=7)
+    plt.barh(y_pos, vs, align='center', alpha=0.5)
+    for a, b in zip(y_pos, vs):
+        plt.text(b+0.5, a-1.5, str(b))
+    plt.yticks(y_pos, ts)
+    plt.xlabel('Data #')
+    plt.ylabel('Categories')
+    plt.xlim([0,max(vs)+1])
+    plt.ylim([-2,y_pos[-1]+2.3])
+    plt.tick_params(axis='y', which='major', labelsize=7)
     plt.title('Looperman Dataset')
 
-    plt.savefig('looperman_dataset_bpm.png', bbox_inches='tight', pad_inches=0.0)
+    plt.savefig('looperman_dataset_categories.png', bbox_inches='tight', pad_inches=0.0)
     #plt.show()
+    plt.close()
 
 if(K_FLAG):
     print('--------------------KEY--------------------')
@@ -144,4 +162,36 @@ if(K_FLAG):
 
     plt.savefig('looperman_dataset_keys.png', bbox_inches='tight', pad_inches=0.0)
     #plt.show()
+    plt.close()
 
+if(B_FLAG):
+    print('--------------------BPM--------------------')
+    # by BPM
+    b = sorted(bpm.items(), key=lambda k:int(k[0]))
+    ts = [t[0] for t in b]
+    vs = [t[1] for t in b]
+    s = 1
+    y_pos = np.arange(0, len(ts)*s, s)
+    
+    print(len(bpm))
+    print(bpm)
+
+    # save text to file
+    with open(out_b, 'w') as o:
+        for j in bpm: o.write(j+','+str(bpm[j])+'\n')
+
+    # Plot - by BPM
+    plt.figure(figsize=(23, 17), num='Looperman Dataset')
+    plt.bar(ts, vs, align='center', alpha=0.5, color='red')
+    for a, b in zip(y_pos, vs):plt.text(a, b+2, str(b), ha='center')
+    #plt.yticks(y_pos, ts)
+    plt.ylabel('Data #')
+    plt.xlabel('BPM')
+    #plt.ylim([0,max(vs)+1])
+    plt.xlim([-2,y_pos[-1]+2.3])
+    plt.tick_params(axis='x', which='major', labelsize=7)
+    plt.title('Looperman Dataset')
+
+    plt.savefig('looperman_dataset_bpm.png', bbox_inches='tight', pad_inches=0.0)
+    #plt.show()
+    plt.close()
